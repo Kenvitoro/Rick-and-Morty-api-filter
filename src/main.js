648b412d121b17305b4de5fc5aa14ccd79ee3ceb
Kenneth Villatoro <kenvittoro@gmail.com>
2022-11-28 21:00:00 -0600

    numeracion funcional

--- src/main.js
+++ src/main.js
@@ -39,7 +39,6 @@ xhr.open("GET", "https://rickandmortyapi.com/api/character");
 
 xhr.send();
 }
-
 getCards();
 
 
@@ -51,7 +50,7 @@ xhr.addEventListener("readystatechange", () => {
   if (xhr.readyState === 4) {
     const maxPages = JSON.parse(xhr.responseText).info.pages;
 
-    console.log(maxPages);
+    // console.log(maxPages);
 
     const pages = [];
 
@@ -65,7 +64,7 @@ xhr.addEventListener("readystatechange", () => {
       }
     };
 
-    console.log(pages);
+    // console.log(pages);
 
     pages.forEach((page,index)=>{
       const number = document.createRange().createContextualFragment(
@@ -85,5 +84,91 @@ xhr.open("GET", "https://rickandmortyapi.com/api/character");
 
 xhr.send();
 }
-
 getPages();
+
+
+function getPagesAll(){
+
+ 
+  
+  const pagesContainer = document.querySelector(".pages");
+
+  
+  pagesContainer.addEventListener("click",(e)=>{
+    const pageClass = e.target.classList.value;
+    const pages = document.querySelectorAll(".number");
+
+    if(pageClass != "number"){
+      return 
+    }
+
+    if(pageClass == "number"){
+
+      pages.forEach((page)=>{
+        page.classList.remove("number-focus")
+      })
+        
+      
+      const page = e.target;
+      // console.log(page);
+
+
+      page.classList.add("number-focus")
+
+
+
+      const xhr = new XMLHttpRequest(),
+      container = document.querySelector(".cards");
+
+
+
+      container.innerHTML = "";
+
+
+
+    
+    xhr.addEventListener("readystatechange", () => {
+      if (xhr.readyState === 4) {
+        const personajes = JSON.parse(xhr.responseText).results;
+    
+        personajes.forEach((personaje) => {
+          const card = document.createRange().createContextualFragment(
+            `<div class="card">
+                    <figure class="card-figure">
+                      <img class="card-img" src="${personaje.image}" alt="">
+                    </figure>
+                    <div class="card-body">
+                      <h3 class="card-name">${personaje.name}</h3>
+                      <span class="card-status" data-live='${personaje.status}'>${personaje.status} - ${personaje.species}</span>
+                      <div class="card-last">
+                        <span>Last known location:</span>
+                        <p class="card-last-location">${personaje.location.name}</p>
+                      </div>
+                      <div class="card-first">
+                        <span>Gender:</span>
+                        <p class="card-first-seen">${personaje.gender}</p>
+                      </div>
+                    </div>
+                  </div>`
+          );
+    
+          container.append(card);
+        });
+      }
+    });
+    
+    xhr.open("GET", `https://rickandmortyapi.com/api/character?page=${e.target.innerText}`);
+    
+    xhr.send();
+      
+    }
+
+
+
+
+  })
+  
+}
+
+getPagesAll();
+
